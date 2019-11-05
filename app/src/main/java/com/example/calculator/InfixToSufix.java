9c6326709efb656887dd6389bff7cab6dc248deb
vonals <2658164762@qq.com>
2019-11-05 21:05:44 +0800

    1.0

---
+++ app/src/main/java/com/example/calculator/InfixToSufix.java
@@ -0,0 +1,208 @@
+package com.example.calculator;
+
+import android.util.Log;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+
+//中缀表达式转后缀
+public class InfixToSufix {
+    public static ArrayList Sufix(StringBuilder str){
+        //识别到"(-"补'0'
+        Log.d("EqualCh1","这里有错吗1");
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)=='-'&&str.charAt(i-1)=='('){
+                str.insert(i,'0');
+            }
+        }
+        StringBuilder temp = new StringBuilder(); // 当做中间字符串，临时存放数字，方便往list中添加
+
+        List<String> list = new ArrayList<>(); // 用于存储分割后的数字和符号
+
+        ArrayList<String> result = new ArrayList<>(); //作为结果集合，后缀表达式保存在其中
+        //以下这部分是中转后的预处理，没啥大用，但是记得改
+        for(int i=0;i<str.length();i++){
+            // 判断是不是数字
+            if ((str.charAt(i) >= '0' && str.charAt(i) <= '9') || str.charAt(i) == '.'){
+                //如果此位为'.'，且这个时候中间字符串为空，自动补0
+                if (str.charAt(i) == '.' && temp.length() == 0){
+                    temp.append(0); // 添加0，在添加'.'
+                    temp.append(str.charAt(i));
+                } else {// 是数字，就往中间字符串添加
+                    temp.append(str.charAt(i));
+                }
+                //最后一位就直接加进去
+                if(i==str.length()-1){
+                    list.add(temp.toString());
+                }
+            }else{//不是数字
+                if(temp.length()!=0){
+                    //如果temp长不为’0‘即有符号，将符号加入list
+                    list.add(temp.toString());
+                }
+                //加入符号
+                list.add(String.valueOf(str.charAt(i)));
+                //清空全部temp串
+                temp.delete(0,temp.length());
+            }
+        }
+        //偷偷输出看下结果ps:话说这个超级遍历好帅啊
+        for(String tList:list){
+            Log.d("EqualCh1",tList + " ");
+        }
+        //定义堆栈，要开始了欧
+        Stack<String> stack = new Stack<>();
+        //在这里定义入栈优先级，加sin/cos时记得用String什么的
+        Map<Character,Integer> map = new HashMap<>();
+        map.put('S',0);
+        map.put('C',0);
+        map.put('(',1);
+        map.put(')',1);
+        map.put('*',2);
+        map.put('/',2);
+        map.put('+',3);
+        map.put('-',3);
+//        map.put('S',4);
+//        map.put('C',4);
+        for(String s:list){
+            //不为数字时
+            if ( s.equals("*") || s.equals("/") || s.equals("+") || s.equals("-") || s.equals("(") || s.equals(")")||s.equals("S")||s.equals("C") ) {
+                if(s.equals("(")){
+                Log.d("EqualCh1","这总到了吧");}
+                if(s.equals(")")){
+                    Log.d("EqualCh1","这也到了");}
+                //没元素时就直接加入
+                if(stack.size()==0){
+                    stack.push(s);
+                }else{//有元素时判断优先级决定下一步（ps:这里改优先级欧）
+                    if(s.equals(")")){
+                        Log.d("EqualCh1","不会没到这吧");
+                        //防一波崩溃，避免空栈操作
+                        if(!stack.empty()){
+                            while(!stack.peek().equals("(")){
+                                Log.d("EqualCh1","不会进不了while吧");
+                                //出栈并add到结果里
+                               result.add( stack.pop());
+                                if(stack.peek().equals("S")||stack.peek().equals("C")){
+                                    Log.d("EqualCh1","进S/C啦");
+                                    result.add(stack.pop());
+                                }
+                                if(stack.empty()){
+                                    break;
+                                }
+                            }
+                            //要是上面一顿出栈后还有并且是'（'就pop掉
+                            if(!stack.empty()){
+                                if(stack.peek().equals("(")){
+                                    stack.pop();
+                                }
+                                if(stack.peek().equals("S")||stack.peek().equals("C")){
+                                    result.add(stack.pop());
+                                    Log.d("EqualCh1","进第二个S/C啦");
+                                }
+                            }
+                        }
+                    }else{
+                        if (s.equals("(")){ //如果是'('将其入栈
+                            stack.push(s);
+                        }else{
+                            if (stack.peek().charAt(0) != '(') {
+                                if (map.get(s.charAt(0)) < map.get(stack.peek().charAt(0))) { //栈顶符号的优先级高于元素优先级，也就是数字小，进栈
+                                    stack.push(s);
+                                }else{
+                                    //栈顶元素优先级低于当前元素，把低的全部输出
+                                    while ((map.get(s.charAt(0)) >= map.get(stack.peek().charAt(0))) && !stack.empty()){
+                                        // System.out.print(stack.pop() + " ");
+                                        //按优先级顺序加入符号
+                                        result.add(stack.pop());
+                                        //防止暴毙
+                                        if (stack.empty()){
+                                            break;
+                                        }
+                                        //有左括号也退出来继续等右括号
+                                        if (stack.peek().equals("(")){
+                                            break;
+                                        }
+                                    }
+                                    stack.push(s);
+                                }
+                            }else{
+                                stack.push(s);
+                            }
+                        }
+                    }
+                }
+            }else{
+                //是数字就直接加入
+                result.add(s);
+            }
+        }
+        //最终都结束了就把栈里全pop了
+        while(!stack.empty()){
+            result.add(stack.pop());
+        }
+        for(int i=0;i<result.size();i++){
+        Log.d("EqualCh1",result.get(i));}
+        return result;
+    }
+    //这里是简单的计算后缀的说
+    public static String Cal(ArrayList arrayList){
+        Log.d("EqualCh1","我大跳2,这里大危机");
+        int length=arrayList.size();
+        String[] arr=new String[length];
+        for(int i=0;i<arrayList.size();i++){
+            arr[i]=(String)arrayList.get(i);
+        }
+        Log.d("asdfg", arrayList.toString());
+        List<String> list=new ArrayList<>();
+        for(String anArr:arr){
+            int size=list.size();
+            switch(anArr){
+                case "+":
+                    BigDecimal a=new BigDecimal(list.remove(size-2)).add(new BigDecimal(list.remove(size - 2)));
+                    list.add(a.stripTrailingZeros().toString()); //去掉结果最后的0
+                    break;
+                case "-":
+                    BigDecimal b=new BigDecimal(list.remove(size-2)).subtract(new BigDecimal(list.remove(size - 2)));
+                    list.add(b.stripTrailingZeros().toString());
+                    break;
+                case "*":
+                    BigDecimal c=new BigDecimal(list.remove(size-2)).multiply(new BigDecimal(list.remove(size - 2)));
+                    list.add(c.stripTrailingZeros().toString());
+                    break;
+                case "/":
+                    BigDecimal d=new BigDecimal(list.remove(size-2)).divide(new BigDecimal(list.remove(size - 2)));
+                    list.add(d.stripTrailingZeros().toString());
+                    break;
+                case "S":
+                    double e=Math.sin(Double.valueOf(list.remove(size-1)));
+                    list.add(""+e);
+                    break;
+                case "C":
+                    double f=Math.cos(Double.valueOf(list.remove(size-1)));
+                    list.add(""+f);
+                    break;
+                //数字就直接加进去
+                default:
+                    list.add(anArr);
+                    break;
+            }
+        }
+        if(list.size()==1){
+            if(list.get(0).length()<30){
+                BigDecimal bd = new BigDecimal(list.get(0));
+                return bd.toPlainString(); //BigDecimal默认直接输出
+            }else{
+                double d = Double.valueOf(list.get(0));
+                return String.valueOf(d); //Double会使用科学计数法输出
+            }
+        }else{
+            return "运算错误";
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,392 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.*;
+import android.widget.*;
+import android.annotation.SuppressLint;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView textview;
+    private StringBuilder sb = new StringBuilder();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //隐藏标题栏
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null){
+            actionBar.hide();
+        }
+        //隐藏通知栏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR);
+        setContentView(R.layout.activity_main);
+        //定义控件
+        Button button0=findViewById(R.id.bt_num0);
+        Button button1=findViewById(R.id.bt_num1);
+        Button button2=findViewById(R.id.bt_num2);
+        Button button3=findViewById(R.id.bt_num3);
+        Button button4=findViewById(R.id.bt_num4);
+        Button button5=findViewById(R.id.bt_num5);
+        Button button6=findViewById(R.id.bt_num6);
+        Button button7=findViewById(R.id.bt_num7);
+        Button button8=findViewById(R.id.bt_num8);
+        Button button9=findViewById(R.id.bt_num9);
+        Button buttonAC = findViewById(R.id.bt_AC);
+        Button buttonDelete = findViewById(R.id.bt_delete);
+        Button buttonDot =  findViewById(R.id.bt_dot);
+        Button buttonEqual =  findViewById(R.id.bt_equal);
+        Button buttonAdd =  findViewById(R.id.bt_add);
+        Button buttonDec =  findViewById(R.id.bt_dec);
+        Button buttonMulti =  findViewById(R.id.bt_multi);
+        Button buttonBracketLeft =  findViewById(R.id.bt_l);
+        Button buttonBracketRight =  findViewById(R.id.bt_r);
+        Button buttonDiv =  findViewById(R.id.bt_div);
+        Button buttonPct = findViewById(R.id.bt_div100);
+        Button buttonSin=findViewById(R.id.bt_sin);
+        Button buttonCos=findViewById(R.id.bt_cos);
+        textview=findViewById(R.id.contentText);
+
+        button0.setOnClickListener(this);
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+        button4.setOnClickListener(this);
+        button5.setOnClickListener(this);
+        button6.setOnClickListener(this);
+        button7.setOnClickListener(this);
+        button8.setOnClickListener(this);
+        button9.setOnClickListener(this);
+        buttonAC.setOnClickListener(this);
+        buttonDelete.setOnClickListener(this);
+        buttonAdd.setOnClickListener(this);
+        buttonBracketLeft.setOnClickListener(this);
+        buttonBracketRight.setOnClickListener(this);
+        buttonDec.setOnClickListener(this);
+        buttonDiv.setOnClickListener(this);
+        buttonDot.setOnClickListener(this);
+        buttonEqual.setOnClickListener(this);
+        buttonMulti.setOnClickListener(this);
+        buttonPct.setOnClickListener(this);
+        buttonSin.setOnClickListener(this);
+        buttonCos.setOnClickListener(this);
+    }
+
+    private int count_negative = 0; //负数标记
+    private boolean equals = false;
+    private int count_bracket_left = 0;
+    private int count_bracket_right = 0;
+
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onClick(View view){
+        switch(view.getId()){
+            //sin
+            case R.id.bt_sin:
+                if (equals) {
+                    sb = sb.delete(0, sb.length());
+                    equals = false;
+                }
+                if (sb.length() > 0 && sb.charAt(sb.length() - 1) == ')') {
+                    sb = sb.append("*S(");
+                } else {
+                    sb = sb.append("S(");
+                }
+                textview.setText(sb.toString());
+                break;
+            //cos
+            case R.id.bt_cos:
+                if (equals) {
+                    sb = sb.delete(0, sb.length());
+                    equals = false;
+                }
+                if (sb.length() > 0 && sb.charAt(sb.length() - 1) == ')') {
+                    sb = sb.append("*C(");
+                } else {
+                    sb = sb.append("C(");
+                }
+                textview.setText(sb.toString());
+                break;
+            //数字
+            case R.id.bt_num0:
+                if (equals) {
+                    sb = sb.delete(0, sb.length());
+                    equals = false;
+                }
+                if (sb.length() > 0 && sb.charAt(sb.length() - 1) == ')') {
+                    sb = sb.append("*0");
+                } else {
+                    sb = sb.append("0");
+                }
+                textview.setText(sb.toString());
+                break;
+            case R.id.bt_num1:
+                if (equals) {
+                    sb = sb.delete(0, sb.length());
+                    equals = false;
+                }
+                if (sb.length() > 0 && sb.charAt(sb.length() - 1) == ')') {
+                    sb = sb.append("*1");
+                } else {
+                    sb = sb.append("1");
+                }
+                textview.setText(sb.toString());
+                break;
+            case R.id.bt_num2:
+                if (equals) {
+                    sb = sb.delete(0, sb.length());
+                    equals = false;
+                }
+                if (sb.length() > 0 && sb.charAt(sb.length() - 1) == ')') {
+                    sb = sb.append("*2");
+                } else {
+                    sb = sb.append("2");
+                }
+                textview.setText(sb.toString());
+                break;
+            case R.id.bt_num3:
+                if (equals) {
+                    sb = sb.delete(0, sb.length());
+                    equals = false;
+                }
+                if (sb.length() > 0 && sb.charAt(sb.length() - 1) == ')') {
+                    sb = sb.append("*3");
+                } else {
+                    sb = sb.append("3");
+                }
+                textview.setText(sb.toString());
+                break;
+            case R.id.bt_num4:
+                if (equals) {
+                    sb = sb.delete(0, sb.length());
+                    equals = false;
+                }
+                if (sb.length() > 0 && sb.charAt(sb.length() - 1) == ')') {
+                    sb = sb.append("*4");
+                } else {
+                    sb = sb.append("4");
+                }
+                textview.setText(sb.toString());
+                break;
+            case R.id.bt_num5:
+                if (equals) {
+                    sb = sb.delete(0, sb.length());
+                    equals = false;
+                }
+                if (sb.length() > 0 && sb.charAt(sb.length() - 1) == ')') {
+                    sb = sb.append("*5");
+                } else {
+                    sb = sb.append("5");
+                }
+                textview.setText(sb.toString());
+                break;
+            case R.id.bt_num6:
+                if (equals) {
+                    sb = sb.delete(0, sb.length());
+                    equals = false;
+                }
+                if (sb.length() > 0 && sb.charAt(sb.length() - 1) == ')') {
+                    sb = sb.append("*6");
+                } else {
+                    sb = sb.append("6");
+                }
+                textview.setText(sb.toString());
+                break;
+            case R.id.bt_num7:
+                if (equals) {
+                    sb = sb.delete(0, sb.length());
+                    equals = false;
+                }
+                if (sb.length() > 0 && sb.charAt(sb.length() - 1) == ')') {
+                    sb = sb.append("*7");
+                } else {
+                    sb = sb.append("7");
+                }
+                textview.setText(sb.toString());
+                break;
+            case R.id.bt_num8:
+                if (equals) {
+                    sb = sb.delete(0, sb.length());
+                    equals = false;
+                }
+                if (sb.length() > 0 && sb.charAt(sb.length() - 1) == ')') {
+                    sb = sb.append("*8");
+                } else {
+                    sb = sb.append("8");
+                }
+                textview.setText(sb.toString());
+                break;
+            case R.id.bt_num9:
+                if (equals) {
+                    sb = sb.delete(0, sb.length());
+                    equals = false;
+                }
+                if (sb.length() > 0 && sb.charAt(sb.length() - 1) == ')') {
+                    sb = sb.append("*9");
+                } else {
+                    sb = sb.append("9");
+                }
+                textview.setText(sb.toString());
+                break;
+            //删除
+            case R.id.bt_delete:
+                if(sb.length()!=0){
+                    sb.deleteCharAt(sb.length()-1);
+                }
+                textview.setText(sb.toString());
+                break;
+            //全清
+            case R.id.bt_AC:
+                sb.delete(0,sb.length());
+                textview.setText(sb.toString());
+                break;
+            //左括号
+            case R.id.bt_l:
+                if (sb.length() > 0 && (sb.charAt(sb.length() - 1) >= '0' && sb.charAt(sb.length() - 1) <= '9')) { //当前面是数字是，自动添加为'*('
+                    sb = sb.append("*(");
+                }
+                if (sb.length() == 0) { //如果此时字符串是空的，也就是说想在式子最前面添加括号，就添加括号
+                    sb = sb.append("(");
+                }
+                if (sb.length() > 0 && (sb.charAt(sb.length() - 1) == '*' || sb.charAt(sb.length() - 1) == '/' || sb.charAt(sb.length() - 1) == '+' || sb.charAt(sb.length() - 1) == '-')) { //如果当括号前面是符号时添加括号
+                    sb = sb.append("(");
+                }
+                textview.setText(sb.toString());
+                break;
+            //右括号
+            case R.id.bt_r:
+                int count_num = 0; //数字的数目
+                count_bracket_left = count_bracket_right = 0;
+                if (sb.length() != 0) {
+                    for (int i = sb.length() - 1; i >= 0; i--) { //对字符串进行遍历，如果存在左括号且括号中有数字，标记转为真,
+                        if (count_bracket_left == 0 && (sb.charAt(i) >= '0' && sb.charAt(i) <= '9')) {
+                            count_num++;
+                        }
+                        if (sb.charAt(i) == '(') {
+                            count_bracket_left++;
+                        }
+                        if (sb.charAt(i) == ')') {
+                            count_bracket_right++;
+                        }
+                    }
+                    Log.d("count_bracket", String.valueOf(count_bracket_left+" "+count_bracket_right));
+                    if ((count_bracket_left > count_bracket_right) && count_num > 0) { //当标记均为真，也就是存在左括号时且在左括号前面有数字，才让添加括号
+                        sb = sb.append(')');
+                    }
+                }
+                textview.setText(sb.toString());
+                break;
+            //除法
+            case R.id.bt_div:
+                if (sb.length() != 0) {
+                    if (sb.charAt(sb.length() - 1) >= '0' && sb.charAt(sb.length() - 1) <= '9') {
+                        sb.append("/");
+                    }
+                }
+                textview.setText(sb.toString());
+                break;
+            //乘法
+            case R.id.bt_multi:
+                if (sb.length() != 0) {
+                    if (sb.charAt(sb.length() - 1) >= '0' && sb.charAt(sb.length() - 1) <= '9') {
+                        sb.append("*");
+                    }
+                }
+                textview.setText(sb.toString());
+                break;
+            //减法
+            case R.id.bt_dec:
+                if (sb.length() != 0) {
+                    if (sb.charAt(sb.length() - 1) >= '0' && sb.charAt(sb.length() - 1) <= '9') {
+                        sb.append("-");
+                    }
+                }
+                textview.setText(sb.toString());
+                break;
+            //加法
+            case R.id.bt_add:
+                if (sb.length() != 0) {
+                    if (sb.charAt(sb.length() - 1) >= '0' && sb.charAt(sb.length() - 1) <= '9'||sb.charAt(sb.length()-1)==')') {
+                        sb.append("+");
+                    }
+                }
+                textview.setText(sb.toString());
+                break;
+             //小数点
+            case R.id.bt_dot:
+                if(sb.length()!=0){
+                    int count_dot=0;
+                    //从后前往遍历，看当前最近数字有多少'.'
+                    for(int i=sb.length()-1;i>=0;i--){
+                        if(sb.charAt(i)=='.'){
+                            count_dot++;
+                        }
+                        //遇到非数字就跳出（见'.'/'+^'跳）
+                        if (!(sb.charAt(i) >= '0' && sb.charAt(i) <= '9')) {
+                            break;
+                        }
+                    }
+                    if(count_dot==0){
+                        //处理例外（加减乘除，括号，根号的0.A）
+                        if(sb.charAt(sb.length() - 1) == '*' || sb.charAt(sb.length() - 1) == '/' || sb.charAt(sb.length() - 1) == '+' || sb.charAt(sb.length() - 1) == '-'|| sb.charAt(sb.length() - 1) == '('){
+                            sb.append("0.");
+                        }else{
+                            sb.append(".");
+                        }
+                    }
+                }
+                textview.setText(sb.toString());
+                break;
+            //等于
+            case R.id.bt_equal:
+                Log.d("EqualCh1","点击了");
+                count_bracket_left=count_bracket_right=0;
+                if(sb.length()!=0) {
+                    //查括号数
+                    Log.d("EqualCh1","进来了");
+                    for (int i = 0; i < sb.length(); i++) {
+                        if (sb.charAt(i) == '(')
+                            count_bracket_left++;
+                        if (sb.charAt(i) == ')')
+                            count_bracket_right++;
+                    }
+                    if (count_bracket_left != count_bracket_right) {
+                        Toast.makeText(MainActivity.this, "请注意括号匹配", Toast.LENGTH_SHORT).show();
+
+                    } else if ((sb.charAt(sb.length() - 1) >= '0' && sb.charAt(sb.length() - 1) <= '9') || sb.charAt(sb.length() - 1) == ')') {
+                        Log.d("EqualCh1","运算！！！");
+                        equals = true;
+                        count_negative = 0;
+                        try {
+                            Log.d("EqualCh1","TRY: "+sb);
+                            textview.setText(InfixToSufix.Cal(InfixToSufix.Sufix(sb)));
+                            sb = sb.delete(0, sb.length());
+                            sb.append(textview.getText().toString());
+                        } catch (Exception e) {
+                            textview.setText("Error");
+//                            Log.d("EqualCh1",e.getMessage());
+                            sb = sb.delete(0, sb.length());
+                        }
+                    }
+                }
+                break;
+            //百分号
+            case R.id.bt_div100:
+                sb.append("%");
+                textview.setText(sb.toString());
+                break;
+            default:
+                break;
+        }
+
+    }
+
+
+}
+
